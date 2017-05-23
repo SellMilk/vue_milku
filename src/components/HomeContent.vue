bc85c2a77f8fefedfd9b6dc16ed5b6682ed267f7
zzzzzzb <503684754@qq.com>
2017-05-23 19:40:30 +0800

    zzb修改

---
+++ src/components/HomeContent.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="index">
+    <header class="home-header">
+      <div class="wrapper">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="找你喜欢的牛奶">
+          <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">查找</button>
+                  </span>
+        </div>
+        <span class="hot-word">伊利</span>
+        <span class="hot-word">蒙牛</span>
+        <span class="hot-word">光明</span>
+        <span class="hot-word">刘颖</span>
+        <span class="hot-word">西域春</span>
+      </div>
+    </header>
+    <div class="home-container">
+      <div class="carousel">
+        <ul class="tips">
+          <li>喝牛奶有助于身体健康喝牛奶有助于身体健康喝牛奶有助于身体健康喝牛奶有助于身体健康</li>
+          <li>早上不宜空腹喝牛奶早上不宜空腹喝牛奶早上不宜空腹喝牛奶早上不宜空腹喝牛奶</li>
+          <li>乳糖不耐症不能喝牛奶</li>
+          <li>喝喝喝喝喝喝</li>
+          <li>这这</li>
+        </ul>
+        <div class="dot">
+          <div class="selected"></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <ul id="product-list">
+        <li class="product">
+          <div class="product-img">
+            <img src="../assets/imgs/xiyuchun.jpg" alt="">
+          </div>
+          <div class="product-describe">
+            <a href="product.html"><h3 class="product-title">西域春牛奶最棒西域春牛奶最棒西域春牛奶最棒</h3></a>
+            <div class="product-price">
+              <span class="num">￥74</span>
+              <span>/月</span>
+            </div>
+            <div class="product-info">
+              <p class="product-date">保质期3天</p>
+              <p class="product-num">2131人订购</p>
+              <p class="product-area">哈尔滨</p>
+            </div>
+          </div>
+        </li>
+        <li class="product">
+          <div class="product-img">
+            <img src="../assets/imgs/xiyuchun.jpg" alt="">
+          </div>
+          <div class="product-describe">
+            <a href="javascript:"><h3 class="product-title">西域春牛奶最棒西域春牛奶最棒西域春牛奶最棒</h3></a>
+            <div class="product-price">
+              <span class="num">￥74</span>
+              <span>/月</span>
+            </div>
+            <div class="product-info">
+              <p class="product-date">保质期3天</p>
+              <p class="product-num">2131人订购</p>
+              <p class="product-area">哈尔滨</p>
+            </div>
+          </div>
+        </li>
+        <li class="product">
+          <div class="product-img">
+            <img src="../assets/imgs/xiyuchun.jpg" alt="">
+          </div>
+          <div class="product-describe">
+            <a href="javascript:"><h3 class="product-title">西域春牛奶最棒西域春牛奶最棒西域春牛奶最棒</h3></a>
+            <div class="product-price">
+              <span class="num">￥74</span>
+              <span>/月</span>
+            </div>
+            <div class="product-info">
+              <p class="product-date">保质期3天</p>
+              <p class="product-num">2131人订购</p>
+              <p class="product-area">哈尔滨</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <p class="down">----------我是有底线的人----------</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  @import "../assets/css/bootstrap.min.css";
+  @import "../assets/css/animate.css";
+  @import "../assets/css/index.css";
+  @import "../assets/css/home.css";
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <comment-footer></comment-footer>
+    <home-content></home-content>
+  </div>
+</template>
+
+<script>
+  import CommentFooter from './CommentFooter'
+  import HomeContent from './HomeContent'
+export default {
+  components: {CommentFooter,HomeContent},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
